13bb79f774125c049d539ae0e2956f0664e49d01
Millicent Lorette <mlorette@et-e5.kzn.21-school.ru>
2021-05-25 18:40:10 +0300

    Finished Day00

---
+++ day00/ex00/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+// using System.Math;
+// using System.DateTime;
+
+namespace ex00
+{
+	struct UserInputData
+	{
+		public double sum;
+		public double rate;
+		public int	term;
+		public int	selectedMonth;
+		public double	payment;
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double	Default;
+			double	LessPayment;
+			double	LessTerm;
+			UserInputData data = new UserInputData();
+
+			if ((args.Length != 5) || (ParseInputData(ref args, ref data) == -1)) {
+				Console.WriteLine("Ошибка ввода. Проверьте входные данные и повторите запрос.");
+				return ;
+			}
+			Console.WriteLine("\nГрафик платеженй без досрочного погашения:");
+			Default = calculate(ref data, 0);
+			Console.WriteLine("\nГрафик платеженй с досрочным погашением с уменьшением платежа:");
+			LessPayment = calculate(ref data, 1);
+			Console.WriteLine("\nГрафик платеженй с досрочным погашением с уменьшением срока:");
+			LessTerm = calculate(ref data, 2);
+			printResult(Default, LessPayment, LessTerm);
+		}
+		static int ParseInputData(ref string[] args, ref UserInputData data)
+		{
+			if (double.TryParse(args[0], out data.sum) == false)
+				return -1;
+			if (data.sum < 0)
+				return -1;
+			if (double.TryParse(args[1], out data.rate) == false)
+				return -1;
+			if (data.rate < 0 || data.rate > 100)
+				return -1;
+			if (int.TryParse(args[2], out data.term) == false)
+				return -1;
+			if (data.term < 0)
+				return -1;
+			if (int.TryParse(args[3], out data.selectedMonth) == false)
+				return -1;
+			if (data.selectedMonth < 0 || data.selectedMonth > data.term)
+				return -1;
+			if (double.TryParse(args[4], out data.payment) == false)
+				return -1;
+			if (data.payment < 0 || data.payment > data.sum)
+				return -1;
+			Console.WriteLine("\nСумма кредита:                                    " + data.sum + "руб.");
+			Console.WriteLine("Годовая процентная ставка:                        " + data.rate + "%");
+			Console.WriteLine("Срок выплаты в месяцах:                           " + data.term + "мес.");
+			Console.WriteLine("Номер месяца в котором вносится досрочный платеж: " + data.selectedMonth + "мес.");
+			Console.WriteLine("Сумма досрочного платежа:                         " + data.payment + "руб.");
+			return 0;
+		}
+		static double	calculate(ref UserInputData data, int flag)
+		{
+			double	InterestRate = data.rate / 12 / 100;
+			double	FormulaPart = Math.Pow(1 + InterestRate, data.term);
+			double	AnnuityPayment = (data.sum * InterestRate * FormulaPart) / (FormulaPart - 1);
+			DateTime CurrentDate = DateTime.Today;
+			int	DaysInCurrentMonth = DateTime.DaysInMonth(CurrentDate.Year, CurrentDate.Month);
+			int	DaysInCurrentYear = 0;
+			for (int i = 1; i <= 12; i++)
+				DaysInCurrentYear += DateTime.DaysInMonth(CurrentDate.Year, i);
+			double MonthlyInterest;
+			double OD = 0;
+			double debt = data.sum;
+			double TotalOverpayed = AnnuityPayment * data.term - data.sum;
+			double RealOverPay = 0;
+			double TotalDebt = TotalOverpayed + data.sum;
+			Console.WriteLine("Дата             Платеж          ОД              Проценты        Остаток долга");
+			for (int cicle = 0; cicle != data.term; cicle++)
+			{
+				DaysInCurrentMonth = DateTime.DaysInMonth(CurrentDate.Year, CurrentDate.Month);
+				MonthlyInterest = (debt * data.rate * DaysInCurrentMonth) / (100 * DaysInCurrentYear);
+				OD = AnnuityPayment - MonthlyInterest;
+				debt -= OD;
+				if ((flag == 1) && (cicle + 1 == data.selectedMonth))
+				{
+					debt -= data.payment;
+					FormulaPart = Math.Pow(1 + InterestRate, data.term - cicle - 1);
+					AnnuityPayment = (debt * InterestRate * FormulaPart) / (FormulaPart - 1);
+				}
+				if ((flag == 2) && (cicle + 1 == data.selectedMonth))
+				{
+					debt -= data.payment;
+					data.term = (int)Math.Log((AnnuityPayment / (AnnuityPayment - InterestRate * debt)), 1 + InterestRate);
+					cicle = -1;
+				}
+				CurrentDate = CurrentDate.AddMonths(1);
+				RealOverPay += MonthlyInterest;
+				if (CurrentDate.Month == 1)
+				{
+					DaysInCurrentYear = 0;
+					for (int j = 1; j <= 12; j++)
+						DaysInCurrentYear += DateTime.DaysInMonth(CurrentDate.Year, j);
+				}
+				Console.Write("01." + String.Format("{0,0:00}", CurrentDate.Month) + "." + CurrentDate.Year + "       ");
+				Console.Write(String.Format("{0,-16:0.00}", AnnuityPayment));
+				Console.Write(String.Format("{0,-16:0.00}", OD));
+				Console.Write(String.Format("{0,-16:0.00}", MonthlyInterest));
+				Console.Write(String.Format("{0,-16:0.00}", debt));
+				Console.WriteLine();
+			}
+			Console.WriteLine("Итоговая переплата: " + String.Format("{0,-16:0.00}", RealOverPay));
+			return RealOverPay;
+		}
+		static void	printResult(double Default, double LessPayment, double LessTerm)
+		{
+			if (LessPayment > LessTerm)
+			{
+				Console.WriteLine("\nПереплата при уменьшении платежа: " + String.Format("{0,0:0.00}", LessPayment));
+				Console.WriteLine("Переплата при уменьшении срока: " + String.Format("{0,0:0.00}", LessTerm));
+				Console.WriteLine("Уменьшение срока выгоднее уменьшения платежа на " + String.Format("{0,0:0.00}", (LessPayment - LessTerm)));
+			}
+			if (LessPayment < LessTerm)
+			{
+				Console.WriteLine("\nПереплата при уменьшении платежа: " + String.Format("{0,0:0.00}", LessPayment));
+				Console.WriteLine("Переплата при уменьшении срока: " + String.Format("{0,0:0.00}", LessTerm));
+				Console.WriteLine("Уменьшение срока выгоднее уменьшения платежа на " + String.Format("{0,0:0.00}", (LessTerm - LessPayment)));
+			}
+			if (LessPayment == LessTerm)
+			{
+				Console.WriteLine("\nПереплата при уменьшении платежа: " + String.Format("{0,0:0.00}", LessPayment));
+				Console.WriteLine("Переплата при уменьшении срока: " + String.Format("{0,0:0.00}", LessTerm));
+				Console.WriteLine("Переплата одинакова в обоих вариантах.");
+			}
+			Console.WriteLine();
+		}
+	}
+}
---
+++ day00/ex01/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+
+namespace ex01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string userName = new string("");
+			if (getInputName(ref userName) == -1)
+			{
+				Console.WriteLine("Validation error.");
+				return ;
+			}
+			for (int i = 0; i <= 2; i++)
+				if (nameCheck(ref userName, i) == 1)
+					return ;
+			Console.WriteLine("Your name was not found.");
+		}
+		static int getInputName(ref string userName)
+		{
+			Console.WriteLine(">Enter name:");
+			userName = Console.ReadLine();
+			if (userName.Length == 0)
+				return -1;
+			int symbCount = 0;
+			for (int i = 0; i < userName.Length; i++)
+				if ((userName[i] >= 'A' && userName[i] <= 'Z') || (userName[i] >= 'a' && userName[i] <= 'z') || userName[i] == ' ' || userName[i] == '-')
+					symbCount++;
+			if (symbCount != userName.Length)
+				return - 1;
+			return 0;
+		}
+		static int LevenshteinDistance(string string1, string string2)
+		{
+			int diff;
+			int[,] m = new int[string1.Length + 1, string2.Length + 1];
+
+			for (int i = 0; i <= string1.Length; i++) {
+				m[i, 0] = i;
+			}
+			for (int j = 0; j <= string2.Length; j++) {
+				m[0, j] = j;
+			}
+			for (int i = 1; i <= string1.Length; i++)
+			{
+				for (int j = 1; j <= string2.Length; j++)
+				{
+					diff = (string1[i - 1] == string2[j - 1]) ? 0 : 1;
+					m[i, j] = Math.Min(Math.Min(m[i - 1, j] + 1, m[i, j - 1] + 1), m[i - 1, j - 1] + diff);
+				}
+			}
+			return m[string1.Length, string2.Length];
+		}
+		static int nameCheck(ref string userName, int distance)
+		{
+			int		LevDistance = 0;
+			string	possibleName;
+			string	userAnswer = new string("");
+			StreamReader sr = new StreamReader("./us.txt");
+			while ((possibleName = sr.ReadLine()) != null)
+			{
+				LevDistance = LevenshteinDistance(userName, possibleName);
+				if (LevDistance == distance)
+				{
+					if (distance == 0)
+					{
+						Console.WriteLine("Hello, " + possibleName);
+						return 1;
+					}
+					while (userAnswer != "Y" && userAnswer != "N")
+					{
+						Console.WriteLine(">Did you mean \"" + possibleName + "\"? Y/N");
+						userAnswer = Console.ReadLine();
+					}
+					if (userAnswer == "Y")
+					{
+						Console.WriteLine("Hello, " + possibleName);
+						return 1;
+					}
+					userAnswer = "";
+				}
+			}
+			return 0;
+		}
+	}
+}
